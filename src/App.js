d15493901f6bd82adb385583b515e6dd4658db3b
Julian Parra <julian.parra@fluxit.com.ar>
2019-09-04 19:35:16 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './bootstrap.min.css';
+import Header from './components/Header';
+import NuevaCita from './components/NuevaCita';
+import ListaCitas from './components/ListaCitas';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    citas: []
+  }
+
+  componentDidMount() {
+    const citasLS = localStorage.getItem('citas');
+    if (citasLS) {
+      this.setState({
+        citas: JSON.parse(citasLS)
+      })
+    }
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem('citas', JSON.stringify(this.state.citas));
+  }
+  crearNuevaCita = datos => {
+    // copiar el state actual
+    const citas = [...this.state.citas, datos];
+
+    // agregar el nuevo state
+    this.setState({
+      citas
+    })
+  }
+
+  eliminarCita = id => {
+    const citasActuales = [...this.state.citas];
+
+    const citas = citasActuales.filter(c => c.id !== id);
+
+    this.setState({
+      citas
+    })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Header
+          titulo='Administrador Pacientes Veterinaria'
+        />
+
+        <div className="row">
+          <div className="col-md-10 mx-auto">
+            <NuevaCita
+              crearNuevaCita={this.crearNuevaCita}
+            />
+          </div>
+
+          <div className="mt-5 col-md-10 mx-auto">
+            <ListaCitas
+              citas={this.state.citas}
+              eliminarCita={this.eliminarCita}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Cita.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+const Cita = ({cita, eliminarCita}) => ( 
+    <div className="media mt-3">
+        <div className="media-body">
+            <h3 className="mt-0">{cita.mascota}</h3>
+            <p className="card-text"><span>Nombre Dueño: </span> {cita.propietario} </p>
+            <p className="card-text"><span>Fecha: </span> {cita.fecha} </p>
+            <p className="card-text"><span>Hora: </span> {cita.hora} </p>
+            <p className="card-text"><span>Sintomas: </span> </p>
+            <p className="card-text">{cita.sintomas}</p>
+        </div>
+        <button onClick= {() => eliminarCita(cita.id)} >Borrar Cita</button>
+    </div>
+);
+
+Cita.propTypes = {
+    cita : PropTypes.array.isRequired,
+    eliminarCita : PropTypes.func.isRequired
+
+}
+
+export default Cita;
